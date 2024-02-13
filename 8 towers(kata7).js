922ff10c32cd331f4fde80f024e6a324d85b19cc
McRais <mcrais21@gmail.com>
2024-02-13 12:31:48 +0300

    Create 8 towers(kata7).js

---
+++ 8 towers(kata7).js
@@ -0,0 +1,12 @@
+function towerCombination(n){
+ function factorialize(num) {
+  if (num < 0) 
+        return -1;
+  else if (num == 0) 
+      return 1;
+  else {
+      return (num * factorialize(num - 1));
+  }
+}
+return factorialize(n);
+}
