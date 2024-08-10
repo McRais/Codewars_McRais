62ff54acaacd58c7ca385d60a7ce2817aad2d822
McRais <mcrais21@gmail.com>
2024-08-10 20:36:03 +0300

    Create My Language Skills(kata7).js

---
+++ kata7/My Language Skills(kata7).js
@@ -0,0 +1,4 @@
+const myLanguages = results => Object.entries(results)
+  .filter(([name, points]) => points >= 60)
+  .sort(([name1, points1], [name2, points2]) => points2 - points1)
+  .map(([name, points]) => name);
